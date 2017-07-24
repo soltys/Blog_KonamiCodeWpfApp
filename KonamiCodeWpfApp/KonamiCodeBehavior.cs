8dbfacafda3bf86cb1ea2816ea80665b293d31ab
Paweł Sołtysiak <pawel@soltysiak.it>
2017-07-24 22:17:15 +0200

    moved display keyboard behavior to seperate file

--- KonamiCodeWpfApp/KonamiCodeBehavior.cs
+++ KonamiCodeWpfApp/KonamiCodeBehavior.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Interactivity;
@@ -56,69 +55,6 @@ namespace KonamiCodeWpfApp
             {
                 currentKey = 0;
             }
-        }
-    }
-
-    class DisplayKeyboardBehavior : Behavior<TextBox>
-    {
-        private Window window;
-
-        protected override void OnAttached()
-        {
-            this.window = Window.GetWindow(this.AssociatedObject);
-
-            if (window ==null)
-            {
-                throw new ArgumentNullException();
-            }
-
-            window.PreviewKeyDown += AssociatedObjectOnKeyDown;
-            
-            base.OnAttached();
-        }
-
-        protected override void OnDetaching()
-        {
-            window.PreviewKeyDown -= AssociatedObjectOnKeyDown;
-            base.OnDetaching();
-        }
-
-        private void AssociatedObjectOnKeyDown(object sender, KeyEventArgs e)
-        {
-            string keyChar = GetChar(e.Key);
-
-            var oldText = this.AssociatedObject.Text;
-            var newText = oldText + keyChar;
-            if (newText.Length > 10)
-            {
-                newText = newText.Substring(newText.Length - 10, 10);
-            }
-            this.AssociatedObject.Text = newText;
-
-        }
-
-        private static string GetChar(Key key)
-        {
-            string keyChar = key.ToString();
-
-            if (key == Key.Up)
-            {
-                keyChar = "↑";
-            }
-            else if (key == Key.Down)
-            {
-                keyChar = "↓";
-            }
-            else if (key == Key.Right)
-            {
-                keyChar = "→";
-            }
-            else if (key == Key.Left)
-            {
-                keyChar = "←";
-            }
-
-            return keyChar;
         }
     }
 }
